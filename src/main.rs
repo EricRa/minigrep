63729373c43670f5d313762da2033dd01a03b6b7
Eric Ransom <6969787+EricRa@users.noreply.github.com>
2024-03-21 00:09:29 -0400

    start 12.4

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+// Starting code from The Rust Book located at:
+// https://doc.rust-lang.org/stable/book/ch12-00-an-io-project.html
+
+// Any changes added by me are MIT licensed as well.
+
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
